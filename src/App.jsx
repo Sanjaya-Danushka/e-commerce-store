db191e4b4772c40026f7eba8010896aab25a432e
Sanjaya Danushka <dsanjaya712@gmail.com>
2025-10-09 13:51:45 +0530

    update links o match router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import HomePage from "./Pages/HomePage";
 import { Routes, Route } from "react-router-dom";
-
+import CheckoutPage from "./Pages/CheckutPage";
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
   );
 };
