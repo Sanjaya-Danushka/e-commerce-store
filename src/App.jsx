01ecf37ab0f6ddd473ea654b0f181961c3835252
Sanjaya Danushka <dsanjaya712@gmail.com>
2025-10-09 13:58:48 +0530

    tracking page created

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import HomePage from "./Pages/HomePage";
 import { Routes, Route } from "react-router-dom";
 import CheckoutPage from "./Pages/CheckutPage";
+import OrdersPage from "./Pages/OrdersPage";
+import TrackingPage from "./Pages/TrackingPage";
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
     </Routes>
   );
 };
